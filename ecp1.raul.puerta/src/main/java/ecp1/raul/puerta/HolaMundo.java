2f73cf5e4e256c7b079d30107a78319aefa9bb5b
raul <raul@raul-VAIO>
2014-10-07 20:32:25 +0200

    CD1

---
+++ ecp1.raul.puerta/src/main/java/ecp1/raul/puerta/HolaMundo.java
@@ -0,0 +1,12 @@
+package ecp1.raul.puerta;
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      System.out.println("CD1");
+		
+		
+	}
+
+}
